2bdab2d31bd42c200a3f717c8fa15b5d8dc3a065
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2024-11-26 17:29:46 +0900

    Add files via upload

--- Create-Board/java/controller/ArticleController.java
+++ Create-Board/java/controller/ArticleController.java
@@ -6,8 +6,12 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @Slf4j // 심플하게 로그찍기
@@ -37,6 +41,35 @@ public class ArticleController {
         log.info(saved.toString());
 //        System.out.println(saved.toString());
 
-        return "";
+        return "redirect:/article/" + saved.getId();
+    }
+
+    @GetMapping("/article/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id = " + id);
+
+        // 1. id로 데이터를 가져옴
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        // 2. 가져온 데이터를 모델에 등록
+        model.addAttribute("article", articleEntity);
+
+        // 3. 보여줄 페이지를 설정!
+        return "article/show";
+
+    }
+
+    @GetMapping("/article")
+    public String index(Model model) {
+        // 1. 모든 Article 가져오기
+        List<Article> articleEntityList = articleRepository.findAll();
+        // 원래는 다운캐스팅 해야함 > 레포에서 findALl을 ArrayList형으로 메서드 오버라이딩 해서 생략 가능
+        // Iterable<Article> articleEntityList = articleEntityList;
+
+        // 2. 가져온 Article 묶음을 View로 전달
+        model.addAttribute("articleList", articleEntityList);
+
+        // 3. View 페이지 생성
+        return "article/index";
     }
 }
--- Create-Board/java/entity/Article.java
+++ Create-Board/java/entity/Article.java
@@ -5,11 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity // DB가 해당 객체 인식 가능
 @AllArgsConstructor
+@NoArgsConstructor // 아규먼트 없는 생성자를 만들겠따!
 @ToString
+@Getter // 모든 것들의 getter 생성
 public class Article {
     @Id // 대표값을 지정! 학번 같이!
     @GeneratedValue // 1, 2, 3,.. 자동 생성 어노테이션 (우왕)
@@ -19,5 +23,6 @@ public class Article {
     private String title;
     @Column
     private String content;
+
 
 }
